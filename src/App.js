e0fd633382fd4e2eadc41e9d3c7f32694a69be26
Aleksandr Marchenko <proshta@gmail.com>
2023-01-10 17:43:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 
 import './App.css';
-import HookUseContext from './hooks/HookUseContext';
-import HookUseReducer from './hooks/HookUseReducer';
-import HookUseRef from './hooks/HookUseRef';
+
 
 
 function App() {
 	return (
 		<div className="App">
 			React
-			<HookUseReducer />
+
 		</div>
 	);
 }
