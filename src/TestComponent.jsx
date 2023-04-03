bc4a61cd3895cd5ba097604639b4daa63bdc05fb
Aleksandr Marchenko <proshta@gmail.com>
2023-04-03 17:19:06 +0300

    Update TestComponent.jsx

--- src/TestComponent.jsx
+++ src/TestComponent.jsx
@@ -3,28 +3,41 @@ import { useEffect } from 'react';
 
 const TestComponent = () => {
 
-	const [count, setCount] = useState(1);
+	const [numParts, setNumParts] = useState(0);
+	const [numRedParts, setNumRedParts] = useState(0);
+	const [floatNum, setFloatNum] = useState(0);
 
+	function handleChangeNumParts(event) {
+		setNumParts(Number(event.target.value));
+	}
 
+	function handleChangeNumRedParts(event) {
+		setNumRedParts(Number(event.target.value));
+	}
 
-	function start() {
-		let interval = setInterval(() => {
-			setCount(count => {
-				if (count < 11) {
-					console.log(`Count change ${count}`)
-					return count + 1;
-				} else {
-					clearInterval(interval);
-					return count;
-				}
-			});
-		}, 5000);
+	function handleChangeFloatNum(event) {
+		setFloatNum(Number(event.target.value));
+	}
+
+	const parts = [];
+	for (let i = 0; i < numParts; i++) {
+		const isRed = i < numRedParts;
+		const color = isRed ? 'red' : 'white';
+		parts.push(<div key={i} style={{ backgroundColor: color }}>{i}</div>);
 	}
 
 	return (
 		<div>
-			<p>Counter: {count}</p>
-			<button onClick={start}>Start</button>
+			<label htmlFor="numParts">Number of Parts:</label>
+			<input type="number" id="numParts" name="numParts" value={numParts} onChange={handleChangeNumParts} />
+
+			<label htmlFor="numRedParts">Number of Red Parts:</label>
+			<input type="number" id="numRedParts" name="numRedParts" value={numRedParts} onChange={handleChangeNumRedParts} />
+
+			<label htmlFor="floatNum">Float Number:</label>
+			<input type="number" id="floatNum" name="floatNum" step="0.01" value={floatNum} onChange={handleChangeFloatNum} />
+
+			<div style={{ display: 'flex' }}>{parts}</div>
 		</div>
 	);
 }
