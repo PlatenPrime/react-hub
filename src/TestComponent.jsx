05eedf42210e907f265c7f45f91c81e24ab7923a
Aleksandr Marchenko <proshta@gmail.com>
2023-04-03 17:41:14 +0300

    Update TestComponent.jsx

--- src/TestComponent.jsx
+++ src/TestComponent.jsx
@@ -2,46 +2,39 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 
 const TestComponent = () => {
-
 	const [numParts, setNumParts] = useState(0);
-	const [numRedParts, setNumRedParts] = useState(0);
 	const [floatNum, setFloatNum] = useState(0);
 
 	function handleChangeNumParts(event) {
 		setNumParts(Number(event.target.value));
 	}
 
-	function handleChangeNumRedParts(event) {
-		setNumRedParts(Number(event.target.value));
-	}
-
 	function handleChangeFloatNum(event) {
 		setFloatNum(Number(event.target.value));
 	}
 
+	const redParts = Math.floor(floatNum * numParts);
+	const whiteParts = numParts - redParts;
 	const parts = [];
+
 	for (let i = 0; i < numParts; i++) {
-		const isRed = i < numRedParts;
-		const color = isRed ? 'red' : 'white';
-		parts.push(<div key={i} style={{ backgroundColor: color }}>{i}</div>);
+		const isRed = i < redParts;
+		const color = isRed ? 'bg-red-500' : 'bg-white';
+		parts.push(<div key={i} className={`w-8 h-8 ${color} border border-gray-500`} />);
 	}
 
 	return (
-		<div>
-			<label htmlFor="numParts">Number of Parts:</label>
-			<input type="number" id="numParts" name="numParts" value={numParts} onChange={handleChangeNumParts} />
+		<div className="flex flex-col items-center">
+			<label htmlFor="numParts" className="mr-2">Number of Parts:</label>
+			<input type="number" id="numParts" name="numParts" value={numParts} onChange={handleChangeNumParts} className="border border-gray-500 px-2 py-1" />
 
-			<label htmlFor="numRedParts">Number of Red Parts:</label>
-			<input type="number" id="numRedParts" name="numRedParts" value={numRedParts} onChange={handleChangeNumRedParts} />
+			<label htmlFor="floatNum" className="ml-2 mr-2">Float Number:</label>
+			<input type="number" id="floatNum" name="floatNum" step="0.001" value={floatNum} onChange={handleChangeFloatNum} className="border border-gray-500 px-2 py-1" />
 
-			<label htmlFor="floatNum">Float Number:</label>
-			<input type="number" id="floatNum" name="floatNum" step="0.01" value={floatNum} onChange={handleChangeFloatNum} />
-
-			<div style={{ display: 'flex' }}>{parts}</div>
+			<div className="flex flex-wrap mt-4">{parts}</div>
 		</div>
 	);
 }
-
 
 
 export default TestComponent;
