6af45f547ba45c166ba5c28fd01baa353506a34f
Aleksandr Marchenko <proshta@gmail.com>
2023-04-04 11:02:22 +0300

    04.04.2023

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
 		<>
 			<TestComponent />
-			<RingComponent />
+			
 		</>
 	);
 }
--- src/RingComponent.jsx
+++ src/RingComponent.jsx
@@ -5,24 +5,37 @@ const RingComponent = () => {
 	const [blueSectorCount, setBlueSectorCount] = useState(2);
 
 	const sectorAngle = 360 / sectorCount;
+	const blueRatio = blueSectorCount / sectorCount;
+	const blueRatioAngle = 360 * blueRatio;
 
 	return (
-		<div className="flex flex-col items-center  ">
-			<svg viewBox="0 0 100 100" className="w-1/3 h-1/3 shadow-2xl  ">
-				{[...Array(sectorCount)].map((_, i) => {
-					const startAngle = sectorAngle * i;
-					const endAngle = startAngle + sectorAngle;
-					const isBlue = i < blueSectorCount;
+		<div className="flex flex-col items-center">
+			<div className="relative w-96 h-96">
+				<svg viewBox="0 0 100 100" className="absolute w-full h-full">
+					{[...Array(sectorCount)].map((_, i) => {
+						const startAngle = sectorAngle * i;
+						const endAngle = startAngle + sectorAngle;
+						const isBlue = i < blueSectorCount;
 
-					return (
-						<path
-							key={i}
-							d={describeSector(50, 50, 40, startAngle, endAngle)}
-							fill={isBlue ? 'red' : 'lightgray'}
-						/>
-					);
-				})}
-			</svg>
+						return (
+							<path
+								key={i}
+								d={describeSector(50, 50, 40, startAngle, endAngle)}
+								fill={isBlue ? 'blue' : 'gray'}
+							/>
+						);
+					})}
+				</svg>
+				<svg viewBox="0 0 100 100" className="absolute w-full h-full">
+					<path
+						d={describeSector(50, 50, 40, 0, blueRatioAngle)}
+						fill="blue"
+					/>
+				</svg>
+				<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-24 h-24 rounded-full bg-white border-2 border-gray-300">
+					<span className="text-xl">{`${(blueRatio * 100).toFixed()}%`}</span>
+				</div>
+			</div>
 			<div className="mt-4 flex space-x-4">
 				<label htmlFor="sectorCountInput">Number of Sectors:</label>
 				<input
@@ -48,6 +61,7 @@ const RingComponent = () => {
 		</div>
 	);
 }
+
 
 function describeSector(x, y, radius, startAngle, endAngle) {
 	const start = polarToCartesian(x, y, radius, endAngle);
