e336d30cd859a916fd1a2d31e640467b0013b5ec
Aleksandr Marchenko <proshta@gmail.com>
2023-05-09 13:01:42 +0300

    Update FetchArts.jsx

--- src/components/FetchArts.jsx
+++ src/components/FetchArts.jsx
@@ -26,7 +26,7 @@ const FetchArts = () => {
 
 
 	const getResponseString = () => {
-		fetch( requestUrl  )
+		fetch(apiUrl)
 			.then(response => response.text())
 			.then(data => {
 				setResponseString(data); // set the response string in state
@@ -38,50 +38,50 @@ const FetchArts = () => {
 
 
 
-const getIndex = () => {
+	const getIndex = () => {
 
-	const findIndex = responseString.indexOf(searchValue);
-	setIndex(findIndex)
-
-
-
-}
-
-
-const getQuant = () => {
-
-	const stringOfQuant = responseString.slice(index, index + 50);
-
-	const numbers = stringOfQuant.match(/\d+/g);
-
-	setQuant(numbers[0])
-
-}
+		const findIndex = responseString.indexOf(searchValue);
+		setIndex(findIndex)
 
 
 
+	}
 
 
-return (
-	<div className='mx-auto w-1/2 flex flex-col  mt-6 space-y-6 ' >
+	const getQuant = () => {
 
-		<input className='border rounded-lg border-blue-800 p-2' value={art} type="text" onChange={(e) => { setArt(e.target.value) }} />
+		const stringOfQuant = responseString.slice(index, index + 50);
 
-		<button onClick={getResponseString} className='text-white bg-rose-800 p-2 w-fit mx-auto' >Сделать запрос по артикулу</button>
+		const numbers = stringOfQuant.match(/\d+/g);
 
-		<button onClick={getIndex} className='text-white bg-teal-800 p-2 w-fit mx-auto' >Найти элемент "{searchValue}"</button>
+		setQuant(numbers[0])
 
-		<button onClick={getQuant} className='text-white bg-blue-600 p-2 w-fit mx-auto' >Получить количество</button>
+	}
 
 
-		<p className='border border-rose-600 p-2' > Артикул:{art}</p>
-		{/* <p>{responseString}</p> */}
-		<p className='border border-rose-600 p-2' > Длина полученной строки HTTP:{responseString.length}</p>
-		<p className='border border-teal-600 p-2' > Индекс элемента "В наявності":{index}</p>
-		<p className='border border-blue-600 p-2' > Количество артикула на остатке:  {quant}</p>
 
-	</div>
-);
+
+
+	return (
+		<div className='mx-auto w-1/2 flex flex-col  mt-6 space-y-6 ' >
+
+			<input className='border rounded-lg border-blue-800 p-2' value={art} type="text" onChange={(e) => { setArt(e.target.value) }} />
+
+			<button onClick={getResponseString} className='text-white bg-rose-800 p-2 w-fit mx-auto' >Сделать запрос по артикулу</button>
+
+			<button onClick={getIndex} className='text-white bg-teal-800 p-2 w-fit mx-auto' >Найти элемент "{searchValue}"</button>
+
+			<button onClick={getQuant} className='text-white bg-blue-600 p-2 w-fit mx-auto' >Получить количество</button>
+
+
+			<p className='border border-rose-600 p-2' > Артикул:{art}</p>
+			{/* <p>{responseString}</p> */}
+			<p className='border border-rose-600 p-2' > Длина полученной строки HTTP:{responseString.length}</p>
+			<p className='border border-teal-600 p-2' > Индекс элемента "В наявності":{index}</p>
+			<p className='border border-blue-600 p-2' > Количество артикула на остатке:  {quant}</p>
+
+		</div>
+	);
 };
 
 export default FetchArts;
