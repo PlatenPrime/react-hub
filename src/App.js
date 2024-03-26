42e04c26eb4c5a96a9a203ce58f060b82a267d50
PlatenPrime <proshta@gmail.com>
2024-03-26 08:36:11 +0200

    26.03.2024

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 import MyEditor from "./components/Editor"
+import ImageUploader from './components/ImageUploader';
 
 
 
@@ -16,9 +17,10 @@ function App() {
 			</header>
 
 			<div
-			className="w-1/2 flex justify-center mx-auto"
+				className="w-1/2 flex justify-center mx-auto"
 			>
-				<MyEditor />
+				{/* <MyEditor /> */}
+				<ImageUploader />
 			</div>
 
 		</div>
---
+++ src/components/ImageUploader.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const ImageUploader = () => {
+	const [imageUrl, setImageUrl] = useState('');
+
+	const uploadImage = async (file) => {
+		const formData = new FormData();
+		formData.append('image', file);
+
+		try {
+			const response = await fetch('https://api.imgur.com/3/image', {
+				method: 'POST',
+				headers: {
+					Authorization: 'Client-ID 86f656ab03b0dcf', // Вставьте ваш Client ID Imgur
+				},
+				body: formData,
+			});
+
+			if (!response.ok) {
+				throw new Error('Failed to upload image');
+			}
+
+			const data = await response.json();
+			setImageUrl(data.data.link);
+		} catch (error) {
+			console.error('Error uploading image to Imgur:', error);
+		}
+	};
+
+	const handleFileInputChange = (event) => {
+		const file = event.target.files[0];
+		if (file) {
+			uploadImage(file);
+		}
+	};
+
+	return (
+		<div
+		className="text-white"
+		>
+			<input type="file" onChange={handleFileInputChange} />
+			<br />
+			<label>Ссылка на загруженное изображение:</label>
+			<p>{imageUrl} </p>
+			<br />
+			{imageUrl && <img src={imageUrl} alt="Uploaded" style={{ maxWidth: '100%' }} />}
+		</div>
+	);
+};
+
+export default ImageUploader;
