9d267735f8baa78d8ede1d6c693e9d7f936d9923
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2024-06-15 19:00:52 +0300

    Update Index.jsx

--- src/components/DndKIT/Index.jsx
+++ src/components/DndKIT/Index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { Reorder } from 'framer-motion';
+import { Reorder, useDragControls } from 'framer-motion';
 
 
 
@@ -14,27 +14,50 @@ export default function Index() {
     console.log(items);
 
 
+
+
     return (
         <div
             className="flex flex-col items-center space-y-4 container mx-auto "
         >
 
+         
+
             <Reorder.Group values={items} onReorder={setItems}>
                 {items.map((el, i) => (
-                    <Reorder.Item   key={el} value={el} >
-                        <div
-                            key={i}
-                            className="w-[200px] p-2 m-4
-                    bg-gradient-to-b from-sky-500 to-sky-900
-                    text-white text-2xl 
-                    grid grid-cols-1 place-content-center rounded-xl"
-                        >
-                            <span>{i}</span>
-                            <span>{el}</span>
-                        </div>
-                    </Reorder.Item>
+                    <ItemWithHandle key={el} el={el} i={i} />
                 ))}
             </Reorder.Group>
         </div>
     )
 }
+
+
+
+
+const ItemWithHandle = ({ el, i }) => {
+
+    const controls = useDragControls()
+
+    return (
+        <Reorder.Item
+            value={el}
+            dragListener={false}
+            dragControls={controls}
+        >
+            <div
+                className=" p-2 m-4
+                    bg-gradient-to-b from-teal-500 to-teal-900
+                    text-white text-2xl 
+                    grid grid-cols-3 place-content-center rounded-xl"
+            >
+                <span>{i}</span>
+                <span>{el}</span>
+                <div
+                    className="rounded-full cursor-pointer p-4 dashed border hover:bg-green-300"
+                    onPointerDown={(e) => controls.start(e)}
+                />
+            </div>
+        </Reorder.Item>
+    )
+}   
